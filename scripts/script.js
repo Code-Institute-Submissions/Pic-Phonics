2ad96eb2cde9c58a345772a3b468e223507bb069
elomasney <eimearlomasney01@gmail.com>
2021-01-03 15:56:08 +0000

    Added some variables to my JS script

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const startButton = document.getElementById('playGame')
+const nextButton = document.getElementById('btn-next')
+const prevButton = document.getElementById('btn-prev')
+const questionContainerElement = document.getElementsByClassName('question-container')
+const questionElement = document.getElementById('question')
+const gameControlsElement = document.getElementById('gameControls')
+
+
