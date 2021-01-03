9462059798f88e2bf4eda5c499a0c7db5e1bf574
elomasney <eimearlomasney01@gmail.com>
2021-01-03 20:44:38 +0000

    Removed choices buttons from HTML and updated questions in JS file

--- scripts/script.js
+++ scripts/script.js
@@ -24,18 +24,117 @@ const questions = {"questions":[
 question:"Which picture has the sound 'T'?",
 choiceA:"tree alt.jpg",
 choiceB:"watermelon.jpg",
-choiceC:"tractor.jpg",
-choiceD:"mouse alt.jpg",
-choiceE:"pumpkin.jpg",
-answer: "choiceB, choiceC"
+choiceC:"mouse alt.jpg",
+choiceD:"pumpkin.jpg",
+answer: "choiceA"
 
 },
 {
 question:"Which picture has the sound A?",
-
+choiceA:"tree alt.jpg",
+choiceB:"watermelon.jpg",
+choiceC:"mouse alt.jpg",
+choiceD:"pumpkin.jpg",
+answer: "choiceA"
 },
 {
 question:"Which picture has the sound S?",
+choiceA:"tree alt.jpg",
+choiceB:"watermelon.jpg",
+choiceC:"mouse alt.jpg",
+choiceD:"pumpkin.jpg",
+answer: "choiceA"
 
-}
+},
+{
+question:"Which picture has the sound M?",
+choiceA:"",
+choiceB:"",
+choiceC:"",
+choiceD:"",
+answer: ""
+
+},
+{
+question:"Which picture has the sound I?",
+choiceA:"",
+choiceB:"",
+choiceC:"",
+choiceD:"",
+answer: ""
+
+},
+{
+question:"Which picture has the sound H?",
+choiceA:"",
+choiceB:"",
+choiceC:"",
+choiceD:"",
+answer: ""
+
+},
+{
+question:"Which picture has the sound C?",
+choiceA:"",
+choiceB:"",
+choiceC:"",
+choiceD:"",
+answer: ""
+
+},
+{
+question:"Which picture has the sound K?",
+choiceA:"",
+choiceB:"",
+choiceC:"",
+choiceD:"",
+answer: ""
+
+},
+{
+question:"Which picture has the sound O?",
+choiceA:"",
+choiceB:"",
+choiceC:"",
+choiceD:"",
+answer: ""
+
+},
+{
+question:"Which picture has the sound P?",
+choiceA:"",
+choiceB:"",
+choiceC:"",
+choiceD:"",
+answer: ""
+
+},
+{
+question:"Which picture has the sound L?",
+choiceA:"",
+choiceB:"",
+choiceC:"",
+choiceD:"",
+answer: ""
+
+},
+{
+question:"Which picture has the sound E?",
+choiceA:"",
+choiceB:"",
+choiceC:"",
+choiceD:"",
+answer: ""
+
+},
+{
+question:"Which picture has the sound G?",
+choiceA:"",
+choiceB:"",
+choiceC:"",
+choiceD:"",
+answer: ""
+
+},
+
 ]};
