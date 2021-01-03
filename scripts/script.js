531353e79fbe7ced35cbb7bc955484fc6d426596
elomasney <eimearlomasney01@gmail.com>
2021-01-03 18:50:33 +0000

    Added an onclick event to play button to start game and also styled the game container

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,41 @@
-const startButton = document.getElementById('playGame')
+const playButton = document.getElementById('playGame')
 const nextButton = document.getElementById('btn-next')
-const prevButton = document.getElementById('btn-prev')
-const questionContainerElement = document.getElementsByClassName('question-container')
-const questionElement = document.getElementById('question')
-const gameControlsElement = document.getElementById('gameControls')
+const gameContainer = document.getElementById('game')
+const question = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answers')
 
+playButton.addEventListener('click', playGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
+function playGame() {
+  playButton.classList.add('hide')
+  document.getElementById("home").classList.add('hide')
+  document.getElementById("overlay").classList.add('hide')
+  currentQuestionIndex = 0
+  gameContainer.classList.remove('hide')
+  setNextQuestion()
+}
+
+const questions = {"questions":[
+{
+question:"Which picture has the sound 'T'?",
+choiceA:"tree alt.jpg",
+choiceB:"watermelon.jpg",
+choiceC:"tractor.jpg",
+choiceD:"mouse alt.jpg",
+choiceE:"pumpkin.jpg",
+answer: "choiceB, choiceC"
+
+},
+{
+question:"Which picture has the sound A?",
+
+},
+{
+question:"Which picture has the sound S?",
+
+}
+]};
