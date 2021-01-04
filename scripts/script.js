f665dfc97b986fa4a78a111bb2e350c07fac7029
elomasney <eimearlomasney01@gmail.com>
2021-01-04 17:02:31 +0000

    Added images to questions array

--- scripts/script.js
+++ scripts/script.js
@@ -4,24 +4,10 @@ const gameContainer = document.getElementById('game')
 const question = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answers')
 
-playButton.addEventListener('click', playGame)
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
-
-function playGame() {
-  playButton.classList.add('hide')
-  document.getElementById("home").classList.add('hide')
-  document.getElementById("overlay").classList.add('hide')
-  currentQuestionIndex = 0
-  gameContainer.classList.remove('hide')
-  setNextQuestion()
-}
-
-const questions = {"questions":[
+const questions = [
 {
 question:"Which picture has the sound 'T'?",
+qimage:"Tt new.jpg",
 choiceA:"tree alt.jpg",
 choiceB:"watermelon.jpg",
 choiceC:"mouse alt.jpg",
@@ -31,6 +17,7 @@ answer: "choiceA"
 },
 {
 question:"Which picture has the sound A?",
+qimage:"Aa new.jpg",
 choiceA:"tree alt.jpg",
 choiceB:"watermelon.jpg",
 choiceC:"mouse alt.jpg",
@@ -39,6 +26,7 @@ answer: "choiceA"
 },
 {
 question:"Which picture has the sound S?",
+qimage:"Ss new.jpg",
 choiceA:"tree alt.jpg",
 choiceB:"watermelon.jpg",
 choiceC:"mouse alt.jpg",
@@ -48,6 +36,7 @@ answer: "choiceA"
 },
 {
 question:"Which picture has the sound M?",
+qimage:"Mm new.jpg",
 choiceA:"",
 choiceB:"",
 choiceC:"",
@@ -57,6 +46,7 @@ answer: ""
 },
 {
 question:"Which picture has the sound I?",
+qimage:"Ii new.jpg",
 choiceA:"",
 choiceB:"",
 choiceC:"",
@@ -66,6 +56,7 @@ answer: ""
 },
 {
 question:"Which picture has the sound H?",
+qimage:"Hh new.jpg",
 choiceA:"",
 choiceB:"",
 choiceC:"",
@@ -75,6 +66,7 @@ answer: ""
 },
 {
 question:"Which picture has the sound C?",
+qimage:"Cc new.jpg",
 choiceA:"",
 choiceB:"",
 choiceC:"",
@@ -84,6 +76,7 @@ answer: ""
 },
 {
 question:"Which picture has the sound K?",
+qimage:"Kk new.jpg",
 choiceA:"",
 choiceB:"",
 choiceC:"",
@@ -93,6 +86,7 @@ answer: ""
 },
 {
 question:"Which picture has the sound O?",
+qimage:"Oo new.jpg",
 choiceA:"",
 choiceB:"",
 choiceC:"",
@@ -102,6 +96,7 @@ answer: ""
 },
 {
 question:"Which picture has the sound P?",
+qimage:"Pp new.jpg",
 choiceA:"",
 choiceB:"",
 choiceC:"",
@@ -111,6 +106,7 @@ answer: ""
 },
 {
 question:"Which picture has the sound L?",
+qimage:"Ll new.jpg",
 choiceA:"",
 choiceB:"",
 choiceC:"",
@@ -120,6 +116,7 @@ answer: ""
 },
 {
 question:"Which picture has the sound E?",
+qimage:"Ee new.jpg",
 choiceA:"",
 choiceB:"",
 choiceC:"",
@@ -129,6 +126,7 @@ answer: ""
 },
 {
 question:"Which picture has the sound G?",
+qimage:"Gg new.jpg",
 choiceA:"",
 choiceB:"",
 choiceC:"",
@@ -136,5 +134,23 @@ choiceD:"",
 answer: ""
 
 },
+]
 
-]};
+
+playButton.addEventListener('click', playGame)
+
+
+function playGame() {
+  playButton.classList.add('hide')
+  document.getElementById("home").classList.add('hide')
+  document.getElementById("overlay").classList.add('hide')
+  currentQuestionIndex = 0
+  gameContainer.classList.remove('hide')
+
+  document.getElementById('question').innerHTML = questions.question;
+  document.getElementById('answers').innerHTML = questions.choiceA + " " + questions.choiceB + " " + questions.choiceC + " " + questions.choiceD + " ";
+}
+
+
+
+
