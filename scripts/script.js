cc984f8de30414a8bfae8b4a2b871bfc585ca47d
elomasney <eimearlomasney01@gmail.com>
2021-01-07 18:48:38 +0000

    Changed CSS styling for h3 and h4 headers in quiz container

--- scripts/script.js
+++ scripts/script.js
@@ -23,13 +23,13 @@ letterText:"This is the letter 'T'.",
 question:"Choose the picture of the word that starts with the letter 'T'?",
 qimage:"assets/images/letters/Tt.jpg",
 choiceA:"assets/images/words/tree.jpg",
-textA:"Tree",
+textA:"TREE",
 choiceB:"assets/images/words/watermelon.jpg",
-textB:"Watermelon",
+textB:"WATERMELON",
 choiceC:"assets/images/words/hippo.jpg",
-textC:"Hippo",
+textC:"HIPPO",
 choiceD:"assets/images/words/pumpkin.jpg",
-textD:"Pumpkin",
+textD:"PUMPKIN",
 answer: "choiceA"
 
 },
@@ -38,13 +38,13 @@ letterText:"This is the letter 'A'.",
 question:"Choose the picture of the word that starts with the letter 'A'?",
 qimage:"assets/images/letters/Aa.jpg",
 choiceA:"assets/images/words/umbrella.jpg",
-textA:"Umbrella",
+textA:"UMBRELLA",
 choiceB:"assets/images/words/grapes.jpg",
-textB:"Grapes",
+textB:"GRAPES",
 choiceC:"assets/images/words/socks.jpg",
-textC:"Socks",
+textC:"SOCKS",
 choiceD:"assets/images/words/apple.jpg",
-textD:"Apple",
+textD:"APPLE",
 answer: "choiceD"
 },
 {
@@ -52,13 +52,13 @@ letterText:"This is the letter 'S'.",
 question:"Choose the picture of the word that starts with the letter 'S'?",
 qimage:"assets/images/letters/Ss.jpg",
 choiceA:"assets/images/words/car.jpg",
-textA:"Car",
+textA:"CAR",
 choiceB:"assets/images/words/snake.jpg",
-textB:"Snake",
+textB:"SNAKE",
 choiceC:"assets/images/words/flower.jpg",
-textC:"Flower",
+textC:"FLOWER",
 choiceD:"assets/images/words/dolphin.jpg",
-textD:"Dolphin",
+textD:"DOLPHIN",
 answer: "choiceB"
 
 },
@@ -67,13 +67,13 @@ letterText:"This is the letter 'M'.",
 question:"Choose the picture of the word that starts with the letter 'M'?",
 qimage:"assets/images/letters/Mm.jpg",
 choiceA:"assets/images/words/rabbit.jpg",
-textA:"Rabbit",
+textA:"RABBIT",
 choiceB:"assets/images/words/carrot.jpg",
-textB:"Carrot",
+textB:"CARROT",
 choiceC:"assets/images/words/mouse.jpg",
-textC:"Mouse",
+textC:"MOUSE",
 choiceD:"assets/images/words/bike.jpg",
-textD:"Bike",
+textD:"BIKE",
 answer: "choiceC"
 
 },
@@ -82,13 +82,13 @@ letterText:"This is the letter 'I'.",
 question:"Choose the picture of the word that starts with the letter 'I'?",
 qimage:"assets/images/letters/Ii.jpg",
 choiceA:"assets/images/words/teddy.jpg",
-textA:"Teddy",
+textA:"TEDDY",
 choiceB:"assets/images/words/books.jpg",
-textB:"Books",
+textB:"BOOKS",
 choiceC:"assets/images/words/duck.jpg",
-textC:"Duck",
+textC:"DUCK",
 choiceD:"assets/images/words/icecream.jpg",
-textD:"Ice-cream",
+textD:"ICE-CREAM",
 answer: "choiceD"
 
 },
@@ -97,13 +97,13 @@ letterText:"This is the letter 'H'.",
 question:"Choose the picture of the word that starts with the letter 'H'?",
 qimage:"assets/images/letters/Hh.jpg",
 choiceA:"assets/images/words/hat.jpg",
-textA:"Hat",
+textA:"HAT",
 choiceB:"assets/images/words/eggs.jpg",
-textB:"Eggs",
+textB:"EGGS",
 choiceC:"assets/images/words/bird.jpg",
-textC:"Bird",
+textC:"BIRD",
 choiceD:"assets/images/words/alligator.jpg",
-textD:"Alligator",
+textD:"ALLIGATOR",
 answer: "choiceA"
 
 },
@@ -112,13 +112,13 @@ letterText:"This is the letter 'C'.",
 question:"Choose the picture of the word that starts with the letter 'C'?",
 qimage:"assets/images/letters/Cc.jpg",
 choiceA:"assets/images/words/dinosaur.jpg",
-textA:"Dinosaur",
+textA:"DINOSAUR",
 choiceB:"assets/images/words/zebra.jpg",
-textB:"Zebra",
+textB:"ZEBRA",
 choiceC:"assets/images/words/cat.jpg",
-textC:"Cat",
+textC:"CAT",
 choiceD:"assets/images/words/squirrel.jpg",
-textD:"Squirrel",
+textD:"SQUIRREL",
 answer: "choiceC"
 
 },
@@ -127,28 +127,28 @@ letterText:"This is the letter 'K'.",
 question:"Choose the picture of the word that starts with the letter 'K'?",
 qimage:"assets/images/letters/Kk.jpg",
 choiceA:"assets/images/words/ball.jpg",
-textA:"Ball",
+textA:"BALL",
 choiceB:"assets/images/words/fox.jpg",
-textB:"Fox",
+textB:"FOX",
 choiceC:"assets/images/words/kite.jpg",
-textC:"Kite",
+textC:"KITE",
 choiceD:"assets/images/words/tiger.jpg",
-textD:"Tiger",
+textD:"TIGER",
 answer: "choiceC"
 
 },
 {
 letterText:"This is the letter 'O'.",
 question:"Choose the picture of the word that starts with the letter 'O'?",
-qimage:"assets/images/letter/Oo.jpg",
+qimage:"assets/images/letters/Oo.jpg",
 choiceA:"assets/images/words/bear.jpg",
-textA:"Bear",
+textA:"BEAR",
 choiceB:"assets/images/words/octopus.jpg",
-textB:"Octopus",
+textB:"OCTOPUS",
 choiceC:"assets/images/words/iguana.jpg",
-textC:"Iguana",
+textC:"IGUANA",
 choiceD:"assets/images/words/jellyfish.jpg",
-textD:"Jellyfish",
+textD:"JELLYFISH",
 answer: "choiceB"
 
 },
@@ -157,13 +157,13 @@ letterText:"This is the letter 'P'.",
 question:"Choose the picture of the word that starts with the letter 'P'?",
 qimage:"assets/images/letters/Pp.jpg",
 choiceA:"assets/images/words/leaf.jpg",
-textA:"Leaf",
+textA:"LEAF",
 choiceB:"assets/images/words/cake.jpg",
-textB:"Cake",
+textB:"CAKE",
 choiceC:"assets/images/words/ladybird.jpg",
-textC:"Ladybird",
+textC:"LADYBIRD",
 choiceD:"assets/images/words/pig.jpg",
-textD:"Pig",
+textD:"PIG",
 answer: "choiceD"
 
 },
@@ -172,13 +172,13 @@ letterText:"This is the letter 'L'.",
 question:"Choose the picture of the word that starts with the letter 'L'?",
 qimage:"assets/images/letters/Ll.jpg",
 choiceA:"assets/images/words/banana.jpg",
-textA:"Banana",
+textA:"BANANA",
 choiceB:"assets/images/words/dog.jpg",
-textB:"Dog",
+textB:"DOG",
 choiceC:"assets/images/words/lion.jpg",
-textC:"Lion",
+textC:"LION",
 choiceD:"assets/images/words/rainbow.jpg",
-textD:"Rainbow",
+textD:"RAINBOW",
 answer: "choiceC"
 
 },
@@ -187,13 +187,13 @@ letterText:"This is the letter 'E'.",
 question:"Choose the picture of the word that starts with the letter 'E'?",
 qimage:"assets/images/letters/Ee.jpg",
 choiceA:"assets/images/words/elephant.jpg",
-textA:"Elephant",
+textA:"ELEPHANT",
 choiceB:"assets/images/words/fish.jpg",
-textB:"Fish",
+textB:"FISH",
 choiceC:"assets/images/words/kangaroo.jpg",
-textC:"Kangaroo",
+textC:"KANGAROO",
 choiceD:"assets/images/words/seal.jpg",
-textD:"Seal",
+textD:"SEAL",
 answer: "choiceA"
 
 },
@@ -202,13 +202,13 @@ letterText:"This is the letter 'G'.",
 question:"Choose the picture of the word that starts with the letter 'G'?",
 qimage:"assets/images/letters/Gg.jpg",
 choiceA:"assets/images/words/penguin.jpg",
-textA:"Penguin",
+textA:"PENGUIN",
 choiceB:"assets/images/words/monkey.jpg",
-textB:"Monkey",
+textB:"MONKEY",
 choiceC:"assets/images/words/orange.jpg",
-textC:"Orange",
+textC:"ORANGE",
 choiceD:"assets/images/words/giraffe.jpg",
-textD:"Giraffe",
+textD:"GIRAFFE",
 answer: "choiceD"
 
 },
@@ -235,25 +235,25 @@ function renderQuestion(){
 
     lText.innerHTML = `<h3>  ${q.letterText}  </h3>`;
 
-    question.innerHTML = `<p> ${q.question} </p>`;
+    question.innerHTML = `<h3> ${q.question} </h3>`;
     
     qImg.innerHTML = `<img src= ${q.qimage} >`;
 
     choiceA.innerHTML = `<img src = ${q.choiceA} >`;
 
-    textA.innerHTML = `<h3> ${q.textA} </h3>`;
+    textA.innerHTML = `<h4> ${q.textA} </h4>`;
 
     choiceB.innerHTML = `<img src = ${q.choiceB} >`;
 
-    textB.innerHTML = `<h3> ${q.textB} </h3>`;
+    textB.innerHTML = `<h4> ${q.textB} </h4>`;
 
     choiceC.innerHTML = `<img src = ${q.choiceC} >`;
 
-    textC.innerHTML = `<h3> ${q.textC} </h3>`;
+    textC.innerHTML = `<h4> ${q.textC} </h4>`;
 
     choiceD.innerHTML = `<img src = ${q.choiceD} >`;
 
-    textD.innerHTML = `<h3> ${q.textD} </h3>`;
+    textD.innerHTML = `<h4> ${q.textD} </h4>`;
 
 }
 
