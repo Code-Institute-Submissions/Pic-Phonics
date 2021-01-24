d9b3cf63bf6fe601409b62ad89c57838f09df1cc
elomasney <eimearlomasney01@gmail.com>
2021-01-24 12:59:18 +0000

    Updated choices container to allow for image click, radio buttons were hidden using CSS and new images were uploaded for the choice answers

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,12 @@ const playLink = document.getElementById("play");
 const exitLink = document.getElementById("leave");
 const submitButton = document.getElementById("submit");
 const gameContainer = document.getElementById('game');
+const questionImageDirectory = "assets/images/letters/";
+const choiceImageDirectory = "assets/images/words/";
 const questionContainer = document.getElementById('question');
 const qImg = document.getElementById("qImage");
 const lText = document.getElementById("letterText");
 const answerContainer = document.getElementById('answers');
-const choices = document.querySelectorAll('input[name="choices"]');
 const TextA = document.getElementById('A');
 const TextB = document.getElementById('B');
 const TextC = document.getElementById('C');
@@ -37,215 +38,330 @@ const questions = [
 {
 letterText:"'T'",
 question:"Choose the picture of the word that starts with the letter 'T'?",
-qimage:"assets/images/letters/Tt.jpg",
+qimage:"Tt.jpg",
 audio: "assets/audio/t.mp3",
-imageA: "assets/images/words/tree.jpg",
-imageB: "assets/images/words/watermelon.jpg",
-imageC: "assets/images/words/hippo.jpg",
-imageD: "assets/images/words/pumpkin.jpg",
-choiceA:"TREE",
-choiceB:"WATERMELON",
-choiceC:"HIPPO",
-choiceD:"PUMPKIN",
-correct: choiceA
-
+choices:[
+    {
+        imageFile:"tree.jpg",
+        choiceText: "TREE",
+    },
+    {
+        imageFile:"watermelon.jpg",
+        choiceText: "WATERMELON",
+    },
+    {
+        imageFile:"hippo.jpg",
+        choiceText: "HIPPO",
+    },
+    {
+        imageFile:"pumpkin.jpg",
+        choiceText: "PUMPKIN",
+    },
+],
+correct:choiceA
 },
 {
 letterText:"'A'",
 question:"Choose the picture of the word that starts with the letter 'A'?",
-qimage: "assets/images/letters/Aa.jpg",
+qimage: "Aa.jpg",
 audio: "assets/audio/a.mp3",
-imageA: "assets/images/words/umbrella.jpg",
-imageB: "assets/images/words/grapes.jpg",
-imageC: "assets/images/words/socks.jpg",
-imageD: "assets/images/words/apple.jpg",
-choiceA: "UMBRELLA",
-choiceB: "GRAPES",
-choiceC: "SOCKS",
-choiceD:"APPLE",
-
-correct:choiceD
+choices: [
+    {
+        imageFile:"umbrella.jpg",
+        choiceText: "UMBRELLA",
+    },
+    {
+        imageFile:"grapes.jpg",
+        choiceText: "GRAPES",
+    },
+    {
+        imageFile:"socks.jpg",
+        choiceText: "SOCKS",
+    },
+    {
+        imageFile:"apple.jpg",
+        choiceText: "APPLE",
+    }
+],
+correct: choiceD
 },
 {
 letterText:"'S'",
 question:"Choose the picture of the word that starts with the letter 'S'?",
-qimage:"assets/images/letters/Ss.jpg",
+qimage:"Ss.jpg",
 audio: "assets/audio/s.mp3",
-imageA: "assets/images/words/car.jpg",
-imageB: "assets/images/words/snake.jpg",
-imageC: "assets/images/words/flower.jpg",
-imageD: "assets/images/words/dolphin.jpg",
-choiceA: "CAR",
-choiceB: "SNAKE",
-choiceC: "FLOWER",
-choiceD: "DOLPHIN",
-
+choices: [
+    {
+        imageFile:"car.jpg",
+        choiceText: "CAR",
+    },
+    {
+        imageFile:"snake.jpg",
+        choiceText: "SNAKE",
+    },
+    {
+        imageFile:"flower.jpg",
+        choiceText: "FLOWER",
+    },
+    {
+        imageFile:"dolphin.jpg",
+        choiceText: "DOLPHIN",
+    }
+],
 correct: choiceB
-
 },
 {
 letterText:"'M'",
 question:"Choose the picture of the word that starts with the letter 'M'?",
-qimage: "assets/images/letters/Mm.jpg",
+qimage: "Mm.jpg",
 audio: "assets/audio/m.mp3",
-imageA: "assets/images/words/rabbit.jpg",
-imageB: "assets/images/words/carrot.jpg",
-imageC: "assets/images/words/mouse.jpg",
-imageD: "assets/images/words/bike.jpg",
-choiceA: "RABBIT",
-choiceB: "CARROT",
-choiceC: "MOUSE",
-choiceD: "BIKE",
-
-correct: choiceC
-
+choices: [
+    {
+        imageFile:"rabbit.jpg",
+        choiceText: "RABBIT"
+    },
+    {
+        imageFile:"carrot.jpg",
+        choiceText: "CARROT"
+    },
+    {
+        imageFile:"mouse.jpg",
+        choiceText: "MOUSE"
+    },
+    {
+        imageFile:"bike.jpg",
+        choiceText: "BIKE"
+    }
+],
+correct:choiceC
 },
 {
 letterText:"'I'",
 question:"Choose the picture of the word that starts with the letter 'I'?",
-qimage:"assets/images/letters/Ii.jpg",
+qimage:"Ii.jpg",
 audio: "assets/audio/i.mp3",
-imageA: "assets/images/words/teddy.jpg",
-imageB: "assets/images/words/books.jpg",
-imageC: "assets/images/words/duck.jpg",
-imageD: "assets/images/words/icecream.jpg",
-choiceA: "TEDDY",
-choiceB: "BOOKS",
-choiceC: "DUCK",
-choiceD: "ICE-CREAM",
-
-correct: choiceD
+choices: [
+    {
+        imageFile:"teddy.jpg",
+        choiceText: "TEDDY"
+    },
+    {
+        imageFile:"books.jpg",
+        choiceText: "BOOKS"
+    },
+    {
+        imageFile:"duck.jpg",
+        choiceText: "DUCK"
+    },
+    {
+        imageFile:"icecream.jpg",
+        choiceText: "ICE-CREAM",
+    }
+],
+correct:choiceD
 },
 {
 letterText:"'H'",
 question:"Choose the picture of the word that starts with the letter 'H'?",
-qimage:"assets/images/letters/Hh.jpg",
+qimage:"Hh.jpg",
 audio: "assets/audio/h.mp3",
-imageA: "assets/images/words/hat.jpg",
-imageB: "assets/images/words/eggs.jpg",
-imageC: "assets/images/words/bird.jpg",
-imageD: "assets/images/words/alligator.jpg",
-choiceA: "HAT",
-choiceB: "EGGS",
-choiceC: "BIRD",
-choiceD: "ALLIGATOR",
-
-correct: choiceA
+choices: [
+    {
+        imageFile:"hat.jpg",
+        choiceText: "HAT"
+    },
+    {
+        imageFile:"eggs.jpg",
+        choiceText: "EGGS"
+    },
+    {
+        imageFile:"bird.jpg",
+        choiceText: "BIRD"
+    },
+    {
+        imageFile:"alligator.jpg",
+        choiceText: "ALLIGATOR",
+    }
+],
+correct:choiceA
 },
 {
 letterText:"'C'",
 question:"Choose the picture of the word that starts with the letter 'C'?",
-qimage:"assets/images/letters/Cc.jpg",
+qimage:"Cc.jpg",
 audio: "assets/audio/c.mp3",
-imageA: "assets/images/words/dinosaur.jpg",
-imageB: "assets/images/words/zebra.jpg",
-imageC: "assets/images/words/cat.jpg",
-imageD: "assets/images/words/squirrel.jpg",
-choiceA: "DINOSAUR",
-choiceB: "ZEBRA",
-choiceC: "CAT",
-choiceD: "SQUIRREL",
-
-correct: choiceC
+choices: [
+    {
+        imageFile:"dinosaur.jpg",
+        choiceText: "DINOSAUR"
+    },
+    {
+        imageFile:"zebra.jpg",
+        choiceText: "ZEBRA"
+    },
+    {
+        imageFile:"cat.jpg",
+        choiceText: "CAT"
+    },
+    {
+        imageFile:"squirrel.jpg",
+        choiceText: "SQUIRREL"
+    }
+],
+correct:choiceC
 },
 {
 letterText:"'K'",
 question:"Choose the picture of the word that starts with the letter 'K'?",
-qimage:"assets/images/letters/Kk.jpg",
+qimage:"Kk.jpg",
 audio: "assets/audio/k.mp3",
-imageA: "assets/images/words/ball.jpg",
-imageB: "assets/images/words/fox.jpg",
-imageC: "assets/images/words/tiger.jpg",
-imageD: "assets/images/words/kite.jpg",
-choiceA: "BALL",
-choiceB: "FOX",
-choiceC: "TIGER",
-choiceD: "KITE",
-
+choices: [
+    {
+        imageFile:"ball.jpg",
+        choiceText: "Ball",
+    },
+    {
+        imageFile:"fox.jpg",
+        choiceText: "FOX",
+    },
+    {
+        imageFile:"tiger.jpg",
+        choiceText: "TIGER",
+    },
+    {
+        imageFile:"kite.jpg",
+        choiceText: "KITE",
+    }
+],
 correct: choiceD
-
 },
 {
 letterText:"'O'",
 question:"Choose the picture of the word that starts with the letter 'O'?",
-qimage:"assets/images/letters/Oo.jpg",
+qimage:"Oo.jpg",
 audio: "assets/audio/o.mp3",
-imageA: "assets/images/words/bear.jpg",
-imageB: "assets/images/words/octopus.jpg",
-imageC: "assets/images/words/iguana.jpg",
-imageD: "assets/images/words/jellyfish.jpg",
-choiceA: "BEAR",
-choiceB: "OCTOPUS",
-choiceC: "IGUANA",
-choiceD: "JELLYFISH",
-
-correct: choiceB
+choices: [
+    {
+        imageFile:"bear.jpg",
+        choiceText: "BEAR",
+    },
+    {
+        imageFile:"octopus.jpg",
+        choiceText: "OCTOPUS",
+    },
+    {
+        imageFile:"iguana.jpg",
+        choiceText: "IGUANA",
+    },
+    {
+        imageFile:"jellyfish.jpg",
+        choiceText: "JELLYFISH",
+    }
+],
+correct:choiceB
 },
 {
 letterText:"'P'",
 question:"Choose the picture of the word that starts with the letter 'P'?",
-qimage:"assets/images/letters/Pp.jpg",
+qimage:"Pp.jpg",
 audio: "assets/audio/p.mp3",
-imageA: "assets/images/words/leaf.jpg",
-imageB: "assets/images/words/cake.jpg",
-imageC: "assets/images/words/ladybird.jpg",
-imageD: "assets/images/words/pig.jpg",
-choiceA: "LEAF",
-choiceB: "CAKE",
-choiceC: "LADYBIRD",
-choiceD: "PIG",
+choices: [
+    {
+        imageFile:"leaf.jpg",
+        choiceText: "LEAF",
+    },
+    {
+        imageFile:"cake.jpg",
+        choiceText: "CAKE",
+    },
+    {
+        imageFile:"ladybird.jpg",
+        choiceText: "LADYBIRD",
+    },
+    {
+        imageFile:"pig.jpg",
+        choiceText: "PIG",
+    }
+],
+correct:choiceD
 
-correct: choiceD
 },
 {
 letterText:"'L'",
 question:"Choose the picture of the word that starts with the letter 'L'?",
-qimage:"assets/images/letters/Ll.jpg",
+qimage:"Ll.jpg",
 audio: "assets/audio/l.mp3",
-imageA: "assets/images/words/lion.jpg",
-imageB: "assets/images/words/banana.jpg",
-imageC: "assets/images/words/dog.jpg",
-imageD: "assets/images/words/rainbow.jpg",
-choiceA: "LION",
-choiceB: "BANANA",
-choiceC: "DOG",
-choiceD: "RAINBOW",
-
-correct: choiceA
+choices: [
+    {
+        imageFile:"lion.jpg",
+        choiceText: "LION"
+    },
+    {
+        imageFile:"banana.jpg",
+        choiceText: "BANANA"
+    },
+    {
+        imageFile:"dog.jpg",
+        choiceText: "DOG"
+    },
+    {
+        imageFile:"rainbow.jpg",
+        choiceText: "RAINBOW",
+    }
+],
+correct:choiceA
 },
 {
 letterText:"'E'",
 question:"Choose the picture of the word that starts with the letter 'E'?",
-qimage:"assets/images/letters/Ee.jpg",
+qimage:"Ee.jpg",
 audio: "assets/audio/e.mp3",
-imageA: "assets/images/words/fish.jpg",
-imageB: "assets/images/words/elephant.jpg",
-imageC: "assets/images/words/kangaroo.jpg",
-imageD: "assets/images/words/seal.jpg",
-choiceA: "FISH",
-choiceB: "ELEPHANT",
-choiceC: "KANGAROO",
-choiceD: "SEAL",
-
-correct: choiceB
+choices: [
+    {
+        imageFile:"fish.jpg",
+        choiceText: "FISH"
+    },
+    {
+        imageFile:"elephant.jpg",
+        choiceText: "ELEPHANT"
+    },
+    {
+        imageFile:"kangaroo.jpg",
+        choiceText: "KANGAROO"
+    },
+    {
+        imageFile:"seal.jpg",
+        choiceText: "SEAL"
+    }
+],
+correct:choiceB
 },
 {
-letterText:"'G'",
-question:"Choose the picture of the word that starts with the letter 'G'?",
-qimage:"assets/images/letters/Gg.jpg",
-audio: "assets/audio/g.mp3",
-imageA: "assets/images/words/penguin.jpg",
-imageB: "assets/images/words/monkey.jpg",
-imageC: "assets/images/words/orange.jpg",
-imageD: "assets/images/words/giraffe.jpg",
-choiceA: "PENGUIN",
-choiceB: "MONKEY",
-choiceC: "ORANGE",
-choiceD: "GIRAFFE",
-
-correct: choiceD
-},
-]
+'letterText':"'G'",
+'question':"Choose the picture of the word that starts with the letter 'G'?",
+'qimage':"Gg.jpg",
+'audio': "assets/audio/g.mp3",
+'choices': [
+    {
+        imageFile:"penguin.jpg",
+        choiceText: "PENGUIN"
+    },
+    {
+        imageFile:"monkey.jpg",
+        choiceText: "MONKEY"
+    },
+    {
+        imageFile:"orange.jpg",
+        choiceText: "ORANGE"
+    },
+    {
+        imageFile:"giraffe.jpg",
+        choiceText: "GIRAFFE"
+    }
+],
+correct:choiceD,
+}
+];
 
 let currentQuestion = 0;
 const lastQuestion = questions.length - 1;
@@ -271,30 +387,34 @@ function renderQuestion(){
     for(let i=0; i<questions.length; i++){
 
     let q = questions[currentQuestion];
-        
+
+     //Questions   
     lText.innerHTML = `<h3> This is the letter ${q.letterText} </h3>`;
     question.innerHTML = `<h3> ${q.question} </h3>`;
-    qImg.innerHTML = `<img src= ${q.qimage} >`;
+    qImg.innerHTML = `<img src= ${questionImageDirectory}${q.qimage} >`;
 
     //Choices
-    imageA.innerHTML = `<img src = ${q.imageA} >`;
-    TextA.innerHTML =  q.choiceA;
-    imageB.innerHTML = `<img src = ${q.imageB} >`;
-    TextB.innerHTML =  q.choiceB;
-    imageC.innerHTML = `<img src = ${q.imageC} >`;
-    TextC.innerHTML =  q.choiceC;
-    imageD.innerHTML = `<img src = ${q.imageD} >`;
-    TextD.innerHTML =  q.choiceD;
-
+    imageA.src = `${choiceImageDirectory}${q.choices[0].imageFile}`;
+    imageA.alt = q.choices[0].choiceText;
+    TextA.innerText =  q.choices[0].choiceText;
+    TextA.classList.add("hide");
+    imageB.src = `${choiceImageDirectory}${q.choices[1].imageFile}`;
+    imageB.alt = q.choices[1].choiceText;
+    TextB.innerText =  q.choices[1].choiceText;
+    TextB.classList.add("hide");
+    imageC.src =` ${choiceImageDirectory}${q.choices[2].imageFile}`;
+    imageC.alt = q.choices[2].choiceText;
+    TextC.innerText = q.choices[2].choiceText;
+    TextC.classList.add("hide");
+    imageD.src = `${choiceImageDirectory}${q.choices[3].imageFile}`;
+    imageD.alt = q.choices[3].choiceText;
+    TextD.innerText = q.choices[3].choiceText;
+    TextD.classList.add("hide");
+    
+    //Score Counter
     scoreCount.innerHTML=`Score: ${score}`;
     
     }
-    //Hides radio button labels text
-    document.getElementById("A").style.display = 'none';
-    document.getElementById("B").style.display = 'none';
-    document.getElementById("C").style.display = 'none';
-    document.getElementById("D").style.display = 'none';
-    //playAudio(questions[currentQuestion].audio);
     setAudio();
 }
 
@@ -304,10 +424,10 @@ function nextQuestion(){
          submitButton.classList.remove('hide');
      }
      else{
-        renderQuestion(questions[currentQuestion++]); //+ questions[currentQuestion].audio)
+        renderQuestion(questions[currentQuestion++]);
         setAudio();
      }
-}
+};
 
 //Functions to locate and play audio files
 function playAudio(file){
@@ -321,52 +441,59 @@ function playAudio(file){
     document.getElementById('sound').setAttribute('onclick', sounds);
   }
 
+const allChoices = document.querySelectorAll(".choice");
+allChoices.forEach((choice) => {
+    choice.addEventListener("click", checkAnswer);
+});
+
 // Checks  if answer users selects is correct or incorrect
 function checkAnswer() {
 
-const correctAnswer = questions[currentQuestion].correct;
-if (document.querySelector('input[name = "choices"]:checked') == correctAnswer){
-    //if (correctAnswer.checked == true){
-    // answer is correct
-    score ++;
-      
-    correct();
-    //Reveals radio button labels when answer is selected
-    document.getElementById("A").style.display = 'block';
-    document.getElementById("B").style.display = 'block';
-    document.getElementById("C").style.display = 'block';
-    document.getElementById("D").style.display = 'block';
-    //Sets timeout delay before next question is rendered
-    setTimeout(function(){
-        nextQuestion();
-    }, 4000); 
+    const correctAnswer = questions[currentQuestion].correct;
+    
+    if (document.querySelector('input[name = "choices"]:checked') == correctAnswer) {
+        // answer is correct
+        score++;
 
-  } else {
-    // answer is incorrect
-    incorrect();
-    document.getElementById("A").style.display = 'block';
-    document.getElementById("B").style.display = 'block';
-    document.getElementById("C").style.display = 'block';
-    document.getElementById("D").style.display = 'block';
-    //Sets a time delay before next question is rendered
-     setTimeout(function(){
-        nextQuestion();
-    }, 4000); 
- 
-  }
+        correct();
+
+        //Reveals radio button labels when answer is selected
+        document.getElementById("A").classList.remove('hide');
+        document.getElementById("B").classList.remove('hide');
+        document.getElementById("C").classList.remove('hide');
+        document.getElementById("D").classList.remove('hide');
+        //Sets timeout delay before next question is rendered
+        setTimeout(function () {
+            nextQuestion();
+        }, 4000);
+
+    } else {
+        // answer is incorrect
+        incorrect();
+
+        document.getElementById("A").classList.remove('hide');
+        document.getElementById("B").classList.remove('hide');
+        document.getElementById("C").classList.remove('hide');
+        document.getElementById("D").classList.remove('hide');
+        //Sets a time delay before next question is rendered
+        setTimeout(function () {
+            nextQuestion();
+        }, 4000);
+
+    }
 }
 
 // Alert if user selects correct answer
 function correct() {
 
-Swal.fire({
-  position: 'center',
-  icon: 'success',
-  title: 'Correct! Well Done!',
-  text:'Keep up the good work!',
-  showConfirmButton: false,
-  timer: 2000
-});
+    Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Correct! Well Done!',
+    text:'Keep up the good work!',
+    showConfirmButton: false,
+    timer: 2000
+    });
  
 }
 
